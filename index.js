e8a147df8f82e90f8feaf721af24fc5e34094725
sultan <sultanjarrar89@gmail.com>
2021-02-04 20:05:04 -0800

    index.html

---
+++ index.js
@@ -0,0 +1,13 @@
+window.sr = ScrollReveal()
+
+window.sr = ScrollReveal()
+sr.reveal('.cover-heading', {
+  duration: 2000,
+  origin: 'top',
+  distance: '300px',
+})
+sr.reveal('.lead', {
+  duration: 2000,
+  origin: 'bottom',
+  distance: '300px',
+})
